5f1cb003fc8649437a5c7841bd58f37865316b0f
Joel Lacerda <joellacerdaol@gmail.com>
2023-10-18 12:07:22 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Joel Lacerda on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
