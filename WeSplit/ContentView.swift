712e8a93d33c4cd3a9fa2765ef0d19dce2da50c2
Joel Lacerda <joellacerdaol@gmail.com>
2023-10-20 18:37:37 -0300

    add variables and form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 10
+    
+    let tipPercentages = [0, 10]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
         }
-        .padding()
     }
 }
 
